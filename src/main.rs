f2dca90869a5956189a4df7d6a9887894ef86f4e
rafiyq <rafiyq@protonmail.com>
2023-01-19 22:52:40 +0700

    training twitter model

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{vec, time::Instant};
-use belajar::nlp::{read_tweets, build_freqs};
+use belajar::{nlp::{read_tweets, build_freqs, extract_features}, optim::gradient_descent, FType};
+use ndarray::{Array2, Array, Axis};
 
 fn main() {
     println!("Loading twitter sample...");
@@ -26,7 +27,7 @@ fn main() {
 
     println!("Building frequency table...");
     let start_build_freqs = Instant::now();
-    let freqs = build_freqs(train_x, train_y);
+    let freqs = build_freqs(&train_x, &train_y);
     println!("\tfreqs length: {}", freqs.keys().len());
     let mut counter = 5;
     for (key, value) in &freqs {
@@ -35,9 +36,29 @@ fn main() {
         counter -= 1;
     }
     println!("Time elapsed {}ms", start_build_freqs.elapsed().as_millis());
+
+    println!("Training the model...");
+    let mut features: Array2<FType> = Array2::zeros((train_x.len(), 3));
+
+    for i in 0..train_x.len() {
+        let feat = extract_features(&train_x[i], &freqs);
+        features[[i, 0]] = feat[0];
+        features[[i, 1]] = feat[1];
+        features[[i, 2]] = feat[2];
+    }
+    let _labels: Array2<FType>= Array::from_vec(train_y).mapv(|x| x as FType).insert_axis(Axis(1));
+
+    let (costs, theta) = gradient_descent(&features, &_labels, Array2::zeros((3, 1)), 1e-9, 1500);
+    println!("The cost after training is {}", costs);
+    println!("The resulting vector of weights is {}", theta);
 }
 
+// use ndarray::{Array2, Axis, array, AssignElem};
 // fn main() {
-//     println!("hello");
-//     println!("{:?}", belajar::stem::get("ion"));
+//     let mut a: Array2<f32> = Array2::zeros((5, 3));
+//     for i in a.axis_iter(Axis(0)) {
+//         // let b = a.axis_iter(Axis(0));
+//         println!("{:?}", i);
+
+//     }
 // }
