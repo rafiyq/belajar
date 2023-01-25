a35c79b67abab3ff248ad4e3f6b5a0c2f0e2bfaf
rafiyq <rafiyq@protonmail.com>
2023-01-25 09:12:52 +0700

    use ndarray as an input

--- src/func/linalg.rs
+++ src/func/linalg.rs
@@ -7,18 +7,14 @@ use crate::FType;
 ///     b: another vector 
 /// Output:
 ///     numerical number representing the cosine similarity between a and b.
-pub fn cosine_similarity_vec(a: Vec<FType>, b: Vec<FType>) -> FType {
-    let a_v = Array1::from_vec(a);
-    let b_v = Array1::from_vec(b);
-    a_v.dot(&b_v) / (a_v.norm_l2() * b_v.norm_l2())
+pub fn cosine_similarity_vec(a: &Array1<FType>, b: &Array1<FType>) -> FType {
+    a.dot(b) / (a.norm_l2() * b.norm_l2())
 }
 /// Input:
-///     A: a numpy array which corresponds to a word vector
-///     B: A numpy array which corresponds to a word vector
+///     a: a vector
+///     b: another vector 
 /// Output:
-///     d: numerical number representing the Euclidean distance between A and B.
-pub fn euclidean_vec(a: Vec<FType>, b: Vec<FType>) -> FType {
-    let x = Array1::from_vec(a);
-    let y = Array1::from_vec(b);
-    (x-y).mapv(|f|f.powi(2)).sum().sqrt()
+///     umerical number representing the Euclidean distance between a and b.
+pub fn euclidean_vec(a: &Array1<FType>, b: &Array1<FType>) -> FType {
+    (a-b).mapv(|f|f.powi(2)).sum().sqrt()
 }
